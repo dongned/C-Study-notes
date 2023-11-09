6b2db48c49368617dcb41bf65c54a819b2035413
dongshiling <dongshiling@wingtech.com>
2023-11-09 20:50:52 +0800

    Day1 study record

--- Day1/Day1/Hello World.cpp
+++ Day1/Day1/Hello World.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 int main()
 {
-	cout << "Hello World" << endl;
+	/*
+	主函数
+	输出Hello World
+	*/
+	cout << "输入姓名" << endl;
+	//暂停,不建议
+	//system("pause");
+	string name;
+
+	cin >> name;
+	
+	//提升用户输入姓名
+	cout << "Hello," << name << endl;
+	//建议使用阻塞
+	cin.get();
+	cin.get();
+
 }
